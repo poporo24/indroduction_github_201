26e505494c5175a5d1acf0bc496604820208cd3a
COMLAB <jpbaltazar23-0368@cca.edu.ph>
2025-02-12 18:18:52 +0800

    sx
    hatdog

---
+++ maggi.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Lupaypay song</title>
+    </head>
+    <body style="text-align: center;">
+
+        <h1>Lupaypay ka girl~ </h1>
+            
+            <img src="mangi.jpg" alt="manggi">
+            <div>
+                
+                <p>Haring Manggi, lupaypay</p>
+                <p>Lupaypay</p>
+                <p>Uuwi ka na lupaypay</p>
+
+                <p>Girl, alam ko naman na sexy ka</p>
+                <p>Maganda ka at kikay ka</p>
+                <p>'Pag ikaw ay aking binira</p>
+                <p>Girl, girl, lupaypay ka</p>
+                <p>Alam ko naman na sexy ka</p>
+                <p>Maganda ka at kikay ka</p>
+                <p>'Pag ikaw ay aking binira</p>
+                <p>Girl, girl, lupaypay ka</p>
+            </div>
+    </body>
+</htm>
